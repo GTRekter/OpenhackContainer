aa249201ccc40f013211340af05737610bfb4a4a
Ivan Porta <ivanporta@microsoft.com>
2021-10-13 18:55:44 +0200

    Add solution

---
+++ azure/deploy.ps1
@@ -0,0 +1,14 @@
+az network vnet subnet list \
+    --resource-group myVnet \
+    --vnet-name myVnet \
+    --query "[0].id" --output tsv
+
+az aks create \
+    --resource-group myResourceGroup \
+    --name myAKSCluster \
+    --network-plugin azure \
+    --vnet-subnet-id <subnet-id> \
+    --docker-bridge-address 172.17.0.1/16 \
+    --dns-service-ip 10.2.0.10 \
+    --service-cidr 10.2.0.0/24 \
+    --generate-ssh-keys
